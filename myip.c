6382945afb478bd710cb76fd8fb1cfb48d75314f
Calisto <46696171+Calistu@users.noreply.github.com>
2019-09-01 23:30:03 -0300

    quiet mode, help screen

--- myip.c
+++ myip.c
@@ -7,7 +7,8 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #define HOST "ifconfig.me"
-int main()
+signed int quiet;
+int main(int argc,char *argv[])
 {
     char *hostname;
     struct hostent *host;
@@ -15,6 +16,8 @@ int main()
     int sockfd, treat,ascii=0,lines=0,pos_a=0,pos_b=0;
 	char externip[15] = "";
     char buffer[400] = "";
+    int args(int,char **);
+    args(argc,argv);
     host = gethostbyname(HOST);
     hostname = inet_ntoa(*(struct in_addr*)host->h_addr_list[0]);
     if(host==NULL)
@@ -22,9 +25,12 @@ int main()
         printf("Cant connect on the searcher host\n");
         exit(1);
     }
-    printf("	----------------------------------------------------\n");
-    printf("	Trying reversing Web connection on %s\n",host->h_name);
-    printf("	IP: %s\n",inet_ntoa(*(struct in_addr*)host->h_addr_list[0]));
+    if(quiet!=1)
+	{
+		printf("	----------------------------------------------------\n");
+		printf("	Trying reversing Web connection on %s\n",host->h_name);
+		printf("	IP: %s\n",inet_ntoa(*(struct in_addr*)host->h_addr_list[0]));
+    }
     ifconfig.sin_family = AF_INET;
     ifconfig.sin_addr.s_addr = inet_addr(hostname); 
     ifconfig.sin_port = htons(80);
@@ -42,10 +48,11 @@ int main()
 		exit(1);
 	}
 	send(sockfd,"GET / HTTP/1.1\nHost: ifconfig.me\n\n",strlen("GET / HTTP/1.1\nHost: ifconfig.me\n\n"),0);
+	if(quiet!=1)
 	printf("	Request sent\n");
 	recv(sockfd,buffer,sizeof(buffer),0);
 	pos_b = strlen(buffer)-15;
-	while(pos_b<strlen(buffer))
+	while(pos_b<=strlen(buffer))
 	{
 		externip[pos_a] = buffer[pos_b];
 		int critic(char);
@@ -57,9 +64,11 @@ int main()
 		pos_a++;
 		pos_b++;
 	}
+	if(quiet!=1)
 	printf("	Your extern ip: ");
-	printf("%s",externip);
-	printf("\n	----------------------------------------------------\n");
+	printf("%s\n",externip);
+	if(quiet!=1)
+	printf("	----------------------------------------------------\n");
 	close(sockfd);
 	exit(1);
 }
@@ -77,4 +86,31 @@ int critic(char ascii)
 			return 0;
 	}
 	return 1;
+}
+int args(int argc,char *argv[])
+{
+	if(argc>1)
+	{	
+		for(int cont=1;cont<argc;cont++)
+		{
+			if(strcmp("-q\n",argv[cont])==0||strcmp("-q",argv[cont])==0)
+			{
+				quiet = 1;
+			}
+			else
+			if(strcmp("-h\n",argv[cont])==0||strcmp("-h",argv[cont])==0)
+			{
+				printf("\n	its simple, ./myip [-q || -h] \n");
+				printf("	-q <quiet> - avoid print messages on screen\n");
+				printf("	-h <help> - show this help screen\n");
+				exit(0); 
+			}
+			else
+			{
+				printf("\n	cant recognize this arg \"%s\"\n",argv[cont]);
+				printf("	use -h for help\n"); 
+				exit(1);
+			}
+		}
+	}
 }
